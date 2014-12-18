e347da73a4a416e69724181883dcbbfeb3845344
Sheldar <kewin.polok@gmail.com>
2014-12-18 23:11:03 +0100

    Added terrains

---
+++ Configuration/Terrains.js
@@ -0,0 +1,588 @@
+[{
+    "Name": "Tundra",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 3,
+        "Sugar": 0,
+        "Tobacco": 0,
+        "Cotton": 0,
+        "Fur": 0,
+        "Lumber": 0,
+        "Ore": 2,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": true,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "Desert",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 2,
+        "Sugar": 0,
+        "Tobacco": 0,
+        "Cotton": 1,
+        "Fur": 0,
+        "Lumber": 0,
+        "Ore": 2,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": true,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "Plains",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 5,
+        "Sugar": 0,
+        "Tobacco": 0,
+        "Cotton": 2,
+        "Fur": 0,
+        "Lumber": 0,
+        "Ore": 1,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": true,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "Prairie",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 3,
+        "Sugar": 0,
+        "Tobacco": 0,
+        "Cotton": 3,
+        "Fur": 0,
+        "Lumber": 0,
+        "Ore": 0,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": true,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "Grassland",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 3,
+        "Sugar": 0,
+        "Tobacco": 3,
+        "Cotton": 0,
+        "Fur": 0,
+        "Lumber": 0,
+        "Ore": 0,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": true,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "Savannah",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 4,
+        "Sugar": 3,
+        "Tobacco": 0,
+        "Cotton": 0,
+        "Fur": 0,
+        "Lumber": 0,
+        "Ore": 0,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": true,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "Marsh",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 3,
+        "Sugar": 0,
+        "Tobacco": 2,
+        "Cotton": 0,
+        "Fur": 0,
+        "Lumber": 0,
+        "Ore": 2,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": true,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "Swamp",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 3,
+        "Sugar": 2,
+        "Tobacco": 0,
+        "Cotton": 0,
+        "Fur": 0,
+        "Lumber": 0,
+        "Ore": 2,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": true,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "BorealForest",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 2,
+        "Sugar": 0,
+        "Tobacco": 0,
+        "Cotton": 0,
+        "Fur": 3,
+        "Lumber": 4,
+        "Ore": 1,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": true,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "ScrubForest",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 2,
+        "Sugar": 0,
+        "Tobacco": 0,
+        "Cotton": 1,
+        "Fur": 2,
+        "Lumber": 2,
+        "Ore": 1,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": true,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "MixedForest",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 3,
+        "Sugar": 0,
+        "Tobacco": 0,
+        "Cotton": 1,
+        "Fur": 3,
+        "Lumber": 6,
+        "Ore": 0,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": true,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "BroadleafForest",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 2,
+        "Sugar": 0,
+        "Tobacco": 0,
+        "Cotton": 1,
+        "Fur": 2,
+        "Lumber": 4,
+        "Ore": 0,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": true,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "ConiferForest",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 2,
+        "Sugar": 0,
+        "Tobacco": 1,
+        "Cotton": 0,
+        "Fur": 2,
+        "Lumber": 6,
+        "Ore": 0,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": true,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "TropicalForest",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 3,
+        "Sugar": 1,
+        "Tobacco": 0,
+        "Cotton": 0,
+        "Fur": 2,
+        "Lumber": 4,
+        "Ore": 0,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": true,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "WetlandForest",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 2,
+        "Sugar": 0,
+        "Tobacco": 1,
+        "Cotton": 0,
+        "Fur": 2,
+        "Lumber": 4,
+        "Ore": 1,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": true,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "RainForest",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 2,
+        "Sugar": 1,
+        "Tobacco": 0,
+        "Cotton": 0,
+        "Fur": 1,
+        "Lumber": 4,
+        "Ore": 1,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": true,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "Arctic",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 0,
+        "Sugar": 0,
+        "Tobacco": 0,
+        "Cotton": 0,
+        "Fur": 0,
+        "Lumber": 0,
+        "Ore": 0,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "Ocean",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 4,
+        "Sugar": 0,
+        "Tobacco": 0,
+        "Cotton": 0,
+        "Fur": 0,
+        "Lumber": 0,
+        "Ore": 0,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": true,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "SeaLane",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 4,
+        "Sugar": 0,
+        "Tobacco": 0,
+        "Cotton": 0,
+        "Fur": 0,
+        "Lumber": 0,
+        "Ore": 0,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "Mountains",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 0,
+        "Sugar": 0,
+        "Tobacco": 0,
+        "Cotton": 0,
+        "Fur": 0,
+        "Lumber": 0,
+        "Ore": 4,
+        "Silver": 1
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": true,
+        "OreDeposit": false
+    }
+},
+{
+    "Name": "Hills",
+    "MoveCost": 2,
+    "Production": {
+        "Food": 2,
+        "Sugar": 0,
+        "Tobacco": 0,
+        "Cotton": 0,
+        "Fur": 0,
+        "Lumber": 0,
+        "Ore": 4,
+        "Silver": 0
+    },
+    "SpecialResource": {
+        "Gems": false,
+        "Oasis": false,
+        "Wheat": false,
+        "PrimeCotton": false,
+        "PrimeTobacco": false,
+        "PrimeSugar": false,
+        "PrimeFurs": false,
+        "Feather": false,
+        "PrimeTimber": false,
+        "Fish": false,
+        "SilverDeposit": false,
+        "OreDeposit": true
+    }
+}]
